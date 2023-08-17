0bd8055d3e0d7b2da0f334e70fe4e7c634eb7ec4
chiranjeevi2411 <pgchiranjeevi@gmail.com>
2023-08-17 08:04:43 +0530

    Added new comment

--- src/com/all/TEstfile.java
+++ src/com/all/TEstfile.java
@@ -3,7 +3,9 @@ package com.all;
 public class TEstfile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		System.out.println("oinzdgoindoinxdoindin");
 
 	}
 
